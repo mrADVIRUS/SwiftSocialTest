e38649d345fe62d2b504246e576f7f13fcdd4ad6
Сергiй Ляховчук <pop_serg@ukr.net>
2017-08-22 17:14:06 +0300

    Settings FireBase complete

--- SocialTest/SignInVC.swift
+++ SocialTest/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,31 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onBtnFacebookPressed(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("JESS: Unable to authentificate with FB - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("JESS: User canceled Facebook authentification")
+            } else {
+                print("JESS: Sucessfully auth with FaceBook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { (user, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("JESS: Sucessfully authenticate with Firebase")
+            }
+        }
+    }
 
 }
 
